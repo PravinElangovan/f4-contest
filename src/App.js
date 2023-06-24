b9c41ac749838747b6cd59a58d320d2b2bf290a9
PravinElangovan <pravinkumarelangovan20@gmail.com>
2023-06-24 22:22:04 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import './App.css';
 
-import Login from './components/Login'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Login from './components/Login';
+import Profile from './components/Profile';
+
 function App() {
   return (
-    <div className="App">
-      <Login />
+    <div className='App'>
+      {<Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      }
     </div>
-  );
+  )
 }
+
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,104 +1,62 @@
-import { useState, useEffect } from 'react';
-import { login, getUserById } from './Api';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+  const [usernameState, setUsernameState] = useState("");
+  const [passwordState, setPasswordState] = useState("");
+  const [errorMsg,setErrMsg]=useState("");
+  const navigate=useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await login(username, password);
-
-      if (response.statusCode === 200) {
-        localStorage.setItem('user', JSON.stringify(response.user));
-        setUser(response.user);
-      } else {
-        setError(response.message);
-      }
-    } catch (error) {
-      setError('An error occurred',error);
-      console.log(error)
+  const handleLogin = () => {
+    if(!usernameState || !passwordState){
+        setErrMsg("All the fields needs to be filled!");
+        return;
     }
-  };
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      const parsedUser = JSON.parse(storedUser);
-      setUser(parsedUser);
-    }
-  }, []);
-
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      if (user && user.id) {
-        try {
-          const response = await getUserById(user.id);
-
-          if (response.statusCode === 200) {
-            localStorage.setItem('user', JSON.stringify(response.user));
-            setUser(response.user);
-          } else {
-            setError(response.message);
-          }
-        } catch (error) {
-          setError('An error occurred');
+    fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: usernameState,
+        password: passwordState,
+      }),
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          return res.json();
         }
-      }
-    };
-
-    fetchUserProfile();
-  }, [user]);
-
-  const handleLogout = () => {
-    localStorage.removeItem('user');
-    setUser(null);
-    
+        else{
+          setErrMsg("Incorrect credentials!");
+          throw new Error("Incorrect credentials!");
+        }
+      })
+      .then((data) =>{
+          localStorage.setItem("id",data.id);
+          localStorage.setItem("token",data.token);
+          navigate("/profile");
+      })
+      .catch((error) => console.log(error));
   };
-
-  if (!user) {
-    return (
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <label>
-            Username:
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <br />
-          <button type="submit">Log in</button>
-        </form>
-        {error && <p>{error}</p>}
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <h2>Profile</h2>
-      <p>ID: {user.id}</p>
-      <p>Username: {user.username}</p>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
-      <button onClick={handleLogout}>Log out</button>
-    </div>
+    <div className="Login">
+      <h1>Login Page</h1>
+    <input
+      type="text"
+      value={usernameState}
+      onChange={(e) => setUsernameState(e.target.value)}
+      placeholder="username"
+    />
+    <input
+      type="password"
+      value={passwordState}
+      onChange={(e) => setPasswordState(e.target.value)}
+      placeholder="password"
+    />
+    <button type="button" onClick={handleLogin}>
+      Login
+    </button>
+    <p className="errorMessage">{errorMsg}</p>
+  </div>
   );
 };
 
